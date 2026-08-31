7ae985d84a97614484a4470501a024bfaba8fa07
simeon0334-cyber <simeon0334@gmail.com>
2026-08-31 11:41:09 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.scenepick.app',
+  appName: 'scenepick',
+  webDir: 'dist'
+};
+
+export default config;
